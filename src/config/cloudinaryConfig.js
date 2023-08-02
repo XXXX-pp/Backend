4c469f7d6d9fa12868a3cb33e6885fe3fdd3e8d7
PraiseJah Moses <praisejah.moses22@gmail.com>
2023-08-02 09:33:20 +0100

    updated env

--- src/config/cloudinaryConfig.js
+++ src/config/cloudinaryConfig.js
@@ -2,9 +2,9 @@
 import {v2 as cloudinary} from 'cloudinary';
 
 cloudinary.config({ 
-    cloud_name: 'dbdbmbyee', 
-    api_key: '551232288565452', 
-    api_secret: 'm9asQFixc7g-egr9R8tCOZFQ56Q' 
+    cloud_name: process.env.CLODINARY_NAME, 
+    api_key: process.env.CLOUDINARY_KEY, 
+    api_secret: process.env.CLODINARY_SECRET 
 });
 
 export default cloudinary;
