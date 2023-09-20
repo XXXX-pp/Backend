3016eda6388e2b90911d5af601255fba73cb1d6b
unknown <osehaeguale@gmail.com>
2023-09-20 17:17:54 +0100

    otp verification

--- src/controllers/auth/signupController.js
+++ src/controllers/auth/signupController.js
@@ -28,18 +28,20 @@ export const createUser = async (req, res) => {
       const user = await saveUser(username.toLowerCase(),email,hashedPassword,posts,postsYouLiked,postsYouSaved)
       
       //send an otp after user creation
-      const otpStatus = await sendUserOtp(user._id,email)
+      const otpStatus = await sendUserOtp(user._id,email,username,password)
       
       //if user is created and otp is sent, send a JSON response
-      if(otpStatus) return res.json({
-        success: true,
+      if(otpStatus.status === false) return res.json({
+        status: 400,
+        message: 'Oops there was an error, OTP could not be sent'
+      });
+      if(otpStatus.status === true) return res.json({
         status: 201,
-        data: user,
-        otpStatus:otpStatus
+        message: 'OTP sent successfully'
       });
     };
   } catch (error) {
     console.log(error);
-    return res.json({ status: 500});
+    return res.json({ status: 500, message: 'Internal Server Error, please try again later'});
   }
 };
--- src/model/otpModel.js
+++ src/model/otpModel.js
@@ -3,6 +3,8 @@ import { Schema, model } from "mongoose";
 const otpSchema = new Schema({
   userId: { type: String, required: true, ref: "User" },
   email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   otp: { type: String, required: true },
   createdAt: { type: Date, required: true },
   expiresAt: { type: Date, required: true },
