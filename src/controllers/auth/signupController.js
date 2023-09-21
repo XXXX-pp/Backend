8a135ab0c693cc23481aa28134a1d02985b32d0d
unknown <osehaeguale@gmail.com>
2023-09-21 15:14:15 +0100

    verify otp

--- src/controllers/auth/signupController.js
+++ src/controllers/auth/signupController.js
@@ -7,16 +7,6 @@ export const createUser = async (req, res) => {
     const { username, password, email } = req.body;
     const {posts,postsYouLiked,postsYouSaved}=[] 
 
-    //check if user already exists
-    // if (userExists) {
-    //   return res.json({
-    //     success: false,
-    //     status: 409,
-    //     message: "User with details already exists",
-    //     data: null,
-    //   });
-    // }
-
     const userExists = await findUser(username,email)
     
     //encrypt user password
@@ -31,17 +21,10 @@ export const createUser = async (req, res) => {
       const otpStatus = await sendUserOtp(user._id,email,username,password)
       
       //if user is created and otp is sent, send a JSON response
-      if(otpStatus.status === false) return res.json({
-        status: 400,
-        message: 'Oops there was an error, OTP could not be sent'
-      });
-      if(otpStatus.status === true) return res.json({
-        status: 201,
-        message: 'OTP sent successfully'
-      });
+      if(otpStatus.status === false) return res.json({ status: 400});
+      if(otpStatus.status === true) return res.json({ status: 201, user: user});
     };
   } catch (error) {
-    console.log(error);
-    return res.json({ status: 500, message: 'Internal Server Error, please try again later'});
+    return res.json({ status: 500});
   }
 };
