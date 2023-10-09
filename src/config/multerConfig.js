35d610ecf6e9792edaf1b469396a4d8ca7afd637
Charles-okoeguale <osehaeguale@gmail.com>
2023-10-09 05:20:03 +0100

    recieve files in server

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,19 +1,44 @@
 import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 
+// Define the upload directory and ensure it exists
+const uploadDirectory = './uploads';
+
+// Ensure the upload directory exists or create it
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+
+// Define a function to generate a unique filename
+const generateUniqueFilename = (file) => {
+  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+  const fileExtension = path.extname(file.originalname);
+  return `${uniqueSuffix}-${file.originalname}`;
+};
+
+// Create the Multer storage configuration
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, uniqueSuffix +'-'+file.originalname)
-    },
-    fileFilter: (req, file, cb)=>{
-      let ext = path.extname(file.originalname)
-      if(ext == '.png' && ext == '.jpg' && ext == '.jpeg' && ext == '.gif'){
-        return cb(null, true);
-      }
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, uploadDirectory);
+  },
+  filename: (req, file, cb) => {
+    cb(null, generateUniqueFilename(file));
+  },
+});
 
-export const upload = multer({storage:storage})
+// Create a file filter function to allow specific file types
+const allowedFileTypes = ['.png', '.jpg', '.jpeg', '.gif'];
+const fileFilter = (req, file, cb) => {
+  const fileExtension = path.extname(file.originalname);
+  if (allowedFileTypes.includes(fileExtension)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type'));
+  }
+};
+
+// Initialize Multer with the storage and file filter configurations
+export const upload = multer({ storage, fileFilter });
+
+export default upload;
--- src/controllers/post/newPostController.js
+++ src/controllers/post/newPostController.js
@@ -5,133 +5,137 @@ import { Readable } from 'stream';
 import * as fs from "fs";
 
 export const createPost = async (req, res) => {
-  try {
-    const requestData = req.body;
-    const user = requestData.username;
-    const description = requestData.description;
+  console.log(req.files)
+  // try {
+  //   const requestData = req.body;
+  //   const user = requestData.username;
+  //   const description = requestData.description;
+  //   console.log(requestData)
 
-    if (requestData.mode === 'camera') {
-      const imageObject = requestData.images || {};
-      const imageKeys = Object.keys(imageObject);
+  //   // if (requestData.mode === 'camera') {
+  //   //   const imageObject = requestData.images || {};
+  //   //   const imageKeys = Object.keys(imageObject);
 
-      if (imageKeys.length === 0) {
-        return res.status(400).send("No image data provided.");
-      }
+  //   //   if (imageKeys.length === 0) {
+  //   //     return res.status(400).send("No image data provided.");
+  //   //   }
 
-      const uploadStatus = [];
-      const uploadPromises = imageKeys.map(async (key) => {
-        const imageData = imageObject[key];
-        const fileBuffer = Buffer.from(imageData, "base64");
-        const publicId = `custom_unique_id_${Date.now()}`;
-        const fileStream = new Readable();
-        fileStream.push(fileBuffer);
-        fileStream.push(null);
+  //   //   const uploadStatus = [];
+  //   //   const uploadPromises = imageKeys.map(async (key) => {
+  //   //     const imageData = imageObject[key];
+  //   //     const fileBuffer = Buffer.from(imageData, "base64");
+  //   //     const publicId = `custom_unique_id_${Date.now()}`;
+  //   //     const fileStream = new Readable();
+  //   //     fileStream.push(fileBuffer);
+  //   //     fileStream.push(null);
 
-        const result = await new Promise((resolve, reject) => {
-          const cloudinaryResponse = cloudinary.uploader.upload_stream(
-            {
-              resource_type: "auto",
-              public_id: publicId,
-            },
-            (error, result) => {
-              if (error) {
-                console.error("Error uploading to Cloudinary:", error);
-                reject(error);
-              } else {
-                resolve(result);
-              }
-            }
-          );
-          fileStream.pipe(cloudinaryResponse);
-        });
+  //   //     const result = await new Promise((resolve, reject) => {
+  //   //       const cloudinaryResponse = cloudinary.uploader.upload_stream(
+  //   //         {
+  //   //           resource_type: "auto",
+  //   //           public_id: publicId,
+  //   //         },
+  //   //         (error, result) => {
+  //   //           if (error) {
+  //   //             console.error("Error uploading to Cloudinary:", error);
+  //   //             reject(error);
+  //   //           } else {
+  //   //             resolve(result);
+  //   //           }
+  //   //         }
+  //   //       );
+  //   //       fileStream.pipe(cloudinaryResponse);
+  //   //     });
 
-        uploadStatus.push(result.secure_url);
-      });
+  //   //     uploadStatus.push(result.secure_url);
+  //   //   });
 
-      await Promise.all(uploadPromises);
+  //   //   await Promise.all(uploadPromises);
 
-      const firstImage = {
-        src: uploadStatus[0],
-        likes: '200',
-        likedBy: [],
-      };
+  //   //   const firstImage = {
+  //   //     src: uploadStatus[0],
+  //   //     likes: '200',
+  //   //     likedBy: [],
+  //   //   };
 
-      const secondImage = {
-        src: uploadStatus[1],
-        likes: '200',
-        likedBy: [],
-      };
+  //   //   const secondImage = {
+  //   //     src: uploadStatus[1],
+  //   //     likes: '200',
+  //   //     likedBy: [],
+  //   //   };
 
-      const newPostStatus = await createNewPost(
-        user.toLowerCase(),
-        description,
-        firstImage,
-        secondImage,
-        '1000',
-        generateUUID
-      );
+  //   //   const newPostStatus = await createNewPost(
+  //   //     user.toLowerCase(),
+  //   //     description,
+  //   //     firstImage,
+  //   //     secondImage,
+  //   //     '1000',
+  //   //     generateUUID
+  //   //   );
 
-      await updateUserPosts(user.toLowerCase(), newPostStatus.postId);
+  //   //   await updateUserPosts(user.toLowerCase(), newPostStatus.postId);
 
-      res.json({
-        success: true,
-        data: {
-          uploadStatus1: uploadStatus[0].created_at,
-          uploadStatus2: uploadStatus[1].created_at,
-        },
-        post: newPostStatus,
-      });
-    } else if (requestData.mode === 'gallery') {
-      // Handle gallery mode here
-      // try {
-      //   const uploadStatus=[]
+  //   //   res.json({
+  //   //     success: true,
+  //   //     data: {
+  //   //       uploadStatus1: uploadStatus[0].created_at,
+  //   //       uploadStatus2: uploadStatus[1].created_at,
+  //   //     },
+  //   //     post: newPostStatus,
+  //   //   });
+  //   // }  
+  //   // if (requestData.mode === 'gallery') {
+  //   //   console.log(requestData)
+  //   //   // Handle gallery mode here
+  //   //   // try {
+  //   //   //   const uploadStatus=[]
   
-      //   for (let i = 0; i < req.files.length; i++) {
-      //     const path = req.files[i].path;
-      //     const originalname = req.files[i].originalname
-      //     const result = await cloudinary.uploader.upload(path, { public_id: originalname + Date.now() });
-      //     fs.unlink(path, (err) => {});
-      //     uploadStatus.push(result)
-      //   }
+  //   //   //   for (let i = 0; i < req.files.length; i++) {
+  //   //   //     const path = req.files[i].path;
+  //   //   //     const originalname = req.files[i].originalname
+  //   //   //     const result = await cloudinary.uploader.upload(path, { public_id: originalname + Date.now() });
+  //   //   //     fs.unlink(path, (err) => {});
+  //   //   //     uploadStatus.push(result)
+  //   //   //   }
     
-      //   const {user,description} = req.body
-      //   const firstImage={
-      //     src:uploadStatus[0].url,
-      //     likes:'200',
-      //     likedBy:[]
-      //   }
-      //   const secondImage={
-      //     src:uploadStatus[1].url,
-      //     likes:'200',
-      //     likedBy:[]
-      //   }
+  //   //   //   const {user,description} = req.body
+  //   //   //   const firstImage={
+  //   //   //     src:uploadStatus[0].url,
+  //   //   //     likes:'200',
+  //   //   //     likedBy:[]
+  //   //   //   }
+  //   //   //   const secondImage={
+  //   //   //     src:uploadStatus[1].url,
+  //   //   //     likes:'200',
+  //   //   //     likedBy:[]
+  //   //   //   }
         
-      //   const newPostStatus = await createNewPost(user.toLowerCase(),description,firstImage,secondImage,'1000',generateUUID)
+  //   //   //   const newPostStatus = await createNewPost(user.toLowerCase(),description,firstImage,secondImage,'1000',generateUUID)
         
-      //   await updateUserPosts(user.toLowerCase(),newPostStatus.postId)
+  //   //   //   await updateUserPosts(user.toLowerCase(),newPostStatus.postId)
         
-      //   res.json({  
-      //     success: true,
-      //     data: {
-      //       uploadStatus1: uploadStatus[0].created_at,
-      //       uploadStatus2: uploadStatus[1].created_at,
-      //     }, 
-      //     post: newPostStatus
-      //   })
-      // }
-      // catch {
-      //   console.log(err);
-      //   res.status(500).json({
-      //     success: false,
-      //     error: err,
-      //   });
-      // }
-    } else {
-      return res.status(400).send("Invalid mode provided.");
-    }
-  } catch (error) {
-    console.error("Error processing images:", error);
-    res.status(500).send("Error processing images.");
-  }
+  //   //   //   res.json({  
+  //   //   //     success: true,
+  //   //   //     data: {
+  //   //   //       uploadStatus1: uploadStatus[0].created_at,
+  //   //   //       uploadStatus2: uploadStatus[1].created_at,
+  //   //   //     }, 
+  //   //   //     post: newPostStatus
+  //   //   //   })
+  //   //   // }
+  //   //   // catch {
+  //   //   //   console.log(err);
+  //   //   //   res.status(500).json({
+  //   //   //     success: false,
+  //   //   //     error: err,
+  //   //   //   });
+  //   //   // }
+  //   // } else {
+  //   //   return res.status(400).send("Invalid mode provided.");
+  //   // }
+  // } catch (error) {
+  //   console.error("Error processing images:", error);
+  //   res.status(500).send("Error processing images.");
+  // }
 };
 
