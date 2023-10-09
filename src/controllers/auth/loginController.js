fb9717e1a28b0642af7bf9b4b4198e2d7eb3218c
PraiseJah Moses <praisejah.moses22@gmail.com>
2023-10-09 19:54:58 +0100

    refactor login controller

--- src/controllers/auth/loginController.js
+++ src/controllers/auth/loginController.js
@@ -3,35 +3,55 @@ import { findUser } from "../../workers/dbWork.js"
 import { generateJwtToken } from "../../utils/utilities.js";
 
 
-//controller to login user
 export async function loginUser(req, res) {
   const { username,email, password } = req.body;
 
   try {
+    //check for request body
+    if (!email || !username || !password) return res.status(404).json({
+      status: 404,
+      message:'Details cannot be empty'
+    }) 
+
     //check if the user exists
     const user = await findUser(username.toLowerCase(),email)
     
     if (!user){
-      return res.json({ success: false, data: null, status: 404 , message: 'User does not exist!' });
+      return res.status(404).json({ 
+        success: false, 
+        data: null, 
+        status: 404 , 
+        message: 'User does not exist!' 
+      });
     }
 
     //if user exists verify the user password
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    
-    if (!passwordMatch) {
-      return res.json({ success: false,  data: null, status: 400, message: 'Invalid credentials' });
-    }
-    
-    //if user password matches send jwt token to login user
-    const token = await generateJwtToken(user)
-    return res.json({
-      status: 200,
-      success: true,
-      data: user,
-      token,
-    });
+    if(user){
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      
+      if (!passwordMatch) {
+        return res.status(401).json({ 
+          success: false,  
+          data: null, 
+          status: 401, 
+          message: 'Invalid credentials' 
+        });
+      }
+
+      //if user password matches send jwt token to login user
+      if (passwordMatch){
+        const token = await generateJwtToken(user)
+        return res.status(200).json({
+          status: 200,
+          success: true,
+          data: user,
+          token
+        })
+      }
+  }
   }catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: `internal server error` });
+    return res.status(500).json({ 
+      message:'Server error, please try again later, '+ error.message
+    });
   }
 }
