0f80f81a441b85315d61e0d306567a024bc39d27
Charles-okoeguale <osehaeguale@gmail.com>
2023-10-13 18:41:39 +0100

    fix camera mode

--- src/controllers/post/newPostController.js
+++ src/controllers/post/newPostController.js
@@ -10,79 +10,77 @@ export const createPost = async (req, res) => {
     const user = requestData.username
     const description = requestData.description
     if (requestData.mode === 'camera') {
+      if (!requestData.images || Object.keys(requestData.images).length === 0) {
+        res.status(400).send("No image data provided.");
+      } else {
       const imageObject = requestData.images || {};
       const imageKeys = Object.keys(imageObject);
+        const uploadStatus = [];
+        const uploadPromises = imageKeys.map(async (key) => {
+          const imageData = imageObject[key];
+          const fileBuffer = Buffer.from(imageData, "base64");
+          const publicId = `custom_unique_id_${Date.now()}`;
+          const fileStream = new Readable();
+          fileStream.push(fileBuffer);
+          fileStream.push(null);
 
-      if (imageKeys.length === 0) {
-        return res.status(400).send("No image data provided.");
-      }
-
-      const uploadStatus = [];
-      const uploadPromises = imageKeys.map(async (key) => {
-        const imageData = imageObject[key];
-        const fileBuffer = Buffer.from(imageData, "base64");
-        const publicId = `custom_unique_id_${Date.now()}`;
-        const fileStream = new Readable();
-        fileStream.push(fileBuffer);
-        fileStream.push(null);
-
-        const result = await new Promise((resolve, reject) => {
-          const cloudinaryResponse = cloudinary.uploader.upload_stream(
-            {
-              resource_type: "auto",
-              public_id: publicId,
-            },
-            (error, result) => {
-              if (error) {
-                console.error("Error uploading to Cloudinary:", error);
-                reject(error);
-              } else {
-                resolve(result);
+          const result = await new Promise((resolve, reject) => {
+            const cloudinaryResponse = cloudinary.uploader.upload_stream(
+              {
+                resource_type: "auto",
+                public_id: publicId,
+              },
+              (error, result) => {
+                if (error) {
+                  console.error("Error uploading to Cloudinary:", error);
+                  reject(error);
+                } else {
+                  resolve(result);
+                }
               }
-            }
-          );
-          fileStream.pipe(cloudinaryResponse);
+            );
+            fileStream.pipe(cloudinaryResponse);
+          });
+
+          uploadStatus.push(result.secure_url);
         });
 
-        uploadStatus.push(result.secure_url);
-      });
+        await Promise.all(uploadPromises);
 
-      await Promise.all(uploadPromises);
+        const firstImage = {
+          src: uploadStatus[0],
+          likes: '200',
+          likedBy: [],
+        };
 
-      const firstImage = {
-        src: uploadStatus[0],
-        likes: '200',
-        likedBy: [],
-      };
+        const secondImage = {
+          src: uploadStatus[1],
+          likes: '200',
+          likedBy: [],
+        };
 
-      const secondImage = {
-        src: uploadStatus[1],
-        likes: '200',
-        likedBy: [],
-      };
+        const newPostStatus = await createNewPost(
+          user.toLowerCase(),
+          description,
+          firstImage,
+          secondImage,
+          '1000',
+          generateUUID
+        );
 
-      const newPostStatus = await createNewPost(
-        user.toLowerCase(),
-        description,
-        firstImage,
-        secondImage,
-        '1000',
-        generateUUID
-      );
-
-      await updateUserPosts(user.toLowerCase(), newPostStatus.postId);
-
-      res.json({
-        success: true,
-        data: {
+        await updateUserPosts(user.toLowerCase(), newPostStatus.postId);
+        res.json({
+          success: true,
+          data: {
           uploadStatus1: uploadStatus[0].created_at,
           uploadStatus2: uploadStatus[1].created_at,
         },
         post: newPostStatus,
-      });
-    }  
-    if (requestData.mode === 'gallery') {
+        });
+      }
+    } else if (requestData.mode === 'gallery') {
       try {
+        // ... (image upload and data processing)
         const uploadStatus=[]
         const user = req.body.username
         const description = req.body.description
@@ -109,17 +107,15 @@ export const createPost = async (req, res) => {
         const newPostStatus = await createNewPost(user.toLowerCase(),description,firstImage,secondImage,'1000',generateUUID)
         
         await updateUserPosts(user.toLowerCase(),newPostStatus.postId)
-        
-        res.json({  
+        res.json({
           success: true,
           data: {
             uploadStatus1: uploadStatus[0].created_at,
             uploadStatus2: uploadStatus[1].created_at,
-          }, 
-          post: newPostStatus
-        })
-      }
-      catch (err) {
+          },
+          post: newPostStatus,
+        });
+      } catch (err) {
         console.log(err);
         res.status(500).json({
           success: false,
@@ -127,8 +123,129 @@ export const createPost = async (req, res) => {
         });
       }
     } else {
-      return res.status(400).send("Invalid mode provided.");
+      res.status(400).send("Invalid mode provided.");
     }
+
+    // if (requestData.mode === 'camera') {
+    //   const imageObject = requestData.images || {};
+    //   const imageKeys = Object.keys(imageObject);
+
+    //   if (imageKeys.length === 0) {
+    //     return res.status(400).send("No image data provided.");
+    //   }
+
+    //   const uploadStatus = [];
+    //   const uploadPromises = imageKeys.map(async (key) => {
+    //     const imageData = imageObject[key];
+    //     const fileBuffer = Buffer.from(imageData, "base64");
+    //     const publicId = `custom_unique_id_${Date.now()}`;
+    //     const fileStream = new Readable();
+    //     fileStream.push(fileBuffer);
+    //     fileStream.push(null);
+
+    //     const result = await new Promise((resolve, reject) => {
+    //       const cloudinaryResponse = cloudinary.uploader.upload_stream(
+    //         {
+    //           resource_type: "auto",
+    //           public_id: publicId,
+    //         },
+    //         (error, result) => {
+    //           if (error) {
+    //             console.error("Error uploading to Cloudinary:", error);
+    //             reject(error);
+    //           } else {
+    //             resolve(result);
+    //           }
+    //         }
+    //       );
+    //       fileStream.pipe(cloudinaryResponse);
+    //     });
+
+    //     uploadStatus.push(result.secure_url);
+    //   });
+
+    //   await Promise.all(uploadPromises);
+
+    //   const firstImage = {
+    //     src: uploadStatus[0],
+    //     likes: '200',
+    //     likedBy: [],
+    //   };
+
+    //   const secondImage = {
+    //     src: uploadStatus[1],
+    //     likes: '200',
+    //     likedBy: [],
+    //   };
+
+    //   const newPostStatus = await createNewPost(
+    //     user.toLowerCase(),
+    //     description,
+    //     firstImage,
+    //     secondImage,
+    //     '1000',
+    //     generateUUID
+    //   );
+
+    //   await updateUserPosts(user.toLowerCase(), newPostStatus.postId);
+
+    //   res.json({
+    //     success: true,
+    //     // data: {
+    //     //   uploadStatus1: uploadStatus[0].created_at,
+    //     //   uploadStatus2: uploadStatus[1].created_at,
+    //     // },
+    //     // post: newPostStatus,
+    //   });
+    // }  
+    // if (requestData.mode === 'gallery') {
+    //   try {
+    //     const uploadStatus=[]
+    //     const user = req.body.username
+    //     const description = req.body.description
+    //     for (let i = 0; i < req.files.length; i++) {
+    //       const path = req.files[i].path;
+    //       const originalname = req.files[i].originalname
+    //       const publicId = `custom_unique_id_${Date.now()}`;
+    //       const result = await cloudinary.uploader.upload(path, { public_id: publicId + Date.now() });
+    //       fs.unlink(path, (err) => {});
+    //       uploadStatus.push(result)
+    //     }
+    
+    //     const firstImage={
+    //       src:uploadStatus[0].url,
+    //       likes:'200',
+    //       likedBy:[]
+    //     }
+    //     const secondImage={
+    //       src:uploadStatus[1].url,
+    //       likes:'200',
+    //       likedBy:[]
+    //     }
+        
+    //     const newPostStatus = await createNewPost(user.toLowerCase(),description,firstImage,secondImage,'1000',generateUUID)
+        
+    //     await updateUserPosts(user.toLowerCase(),newPostStatus.postId)
+        
+    //     res.json({  
+    //       success: true,
+    //       data: {
+    //         uploadStatus1: uploadStatus[0].created_at,
+    //         uploadStatus2: uploadStatus[1].created_at,
+    //       }, 
+    //       post: newPostStatus
+    //     })
+    //   }
+    //   catch (err) {
+    //     console.log(err);
+    //     res.status(500).json({
+    //       success: false,
+    //       error: err,
+    //     });
+    //   }
+    // } else {
+    //   return res.status(400).send("Invalid mode provided.");
+    // }
   } catch (error) {
     console.error("Error processing images:", error);
     res.status(500).send("Error processing images.");
