9f8c61d296e4a7aec8c69238ba4e22ed8285cfe9
PraiseJah <praisejah.moses22@gmail.com>
2023-10-15 09:59:19 +0100

    refactored code to handle authentication logical errors

--- src/workers/otpWork.js
+++ src/workers/otpWork.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs"
-import { findOtp,saveOtp } from "../workers/dbWork.js";
+import { findOtp,saveOtp,findUser } from "../workers/dbWork.js";
+
 
 export const issueOtp = async (userId, email, username, password) => {
   const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
@@ -15,11 +16,13 @@ export const issueOtp = async (userId, email, username, password) => {
 };
 
 export const verifyOtp = async (email, otp) => {
+  const userVerified= await findUser("",email)
+  if (userVerified.isVerified) return {status:true, isVerified:userVerified.isVerified}
   const otpDetails= await findOtp(email)
 
-  if(!otpDetails){return false}
+  if(!otpDetails) return {status:false, otpDetails:false}
 
   const validOtp = await bcrypt.compare(otp, otpDetails.otp)
-  if (!validOtp) return false;
-  if (validOtp) return true
+  if (!validOtp) return {status:false, validOtp:false}
+  if (validOtp) return {status:true, validOtp:true}
 };
